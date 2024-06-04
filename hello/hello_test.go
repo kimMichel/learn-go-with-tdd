79d495bf28c333a94ee95db75a94ccd79fe3af9f
Michel Kim <kim98.michel@gmail.com>
2024-06-04 15:30:25 -0300

    refactor hello func/test

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello("Michel")
-	expected := "Hello, Michel"
-
-	if result != expected {
-		t.Errorf("result: '%s', expected: '%s'", result, expected)
+	checkCorrectText := func(t *testing.T, result, expected string) {
+		t.Helper()
+		if result != expected {
+			t.Errorf("result: '%s', expected: '%s'", result, expected)
+		}
 	}
+
+	t.Run("say hello to people", func(t *testing.T) {
+		result := Hello("Michel")
+		expected := "Hello Michel"
+
+		checkCorrectText(t, result, expected)
+	})
+
+	t.Run("say 'Hello World' when the parameter is empty", func (t *testing.T) {
+		result := Hello("")
+		expected := "Hello World"
+
+		checkCorrectText(t, result, expected)
+	})
 }
