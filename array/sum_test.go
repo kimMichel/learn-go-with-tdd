92103c0616fd266a2b4e8ef98faf5e3292fe02d6
Michel Kim <kim98.michel@gmail.com>
2024-06-04 21:24:48 -0300

    chage array to slice

--- array/sum_test.go
+++ array/sum_test.go
@@ -3,12 +3,16 @@ package array
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	result := Sum(numbers)
-	expect := 15
+	t.Run("collection of any size of numbers", func(t *testing.T) {
+		numbers := []int{ 1, 2, 3, 4, 5}
 
-	if result != expect {
-		t.Errorf("numbers: '%v', expected: '%v', result: '%v'", numbers, expect, result)
-	}
+		result := Sum(numbers)
+		expect := 15
+
+		if result != expect {
+			t.Errorf("numbers: '%v', expected: '%v', result: '%v'", numbers, expect, result)
+		}
+	})
+
 }
