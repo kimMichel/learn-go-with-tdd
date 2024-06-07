c13127f333f0df085ac6c457205b276c9b9fa0bc
Michel Kim <kim98.michel@gmail.com>
2024-06-06 22:24:07 -0300

    refactor area test

--- structure-methods-interfaces/area_test.go
+++ structure-methods-interfaces/area_test.go
@@ -3,28 +3,18 @@ package structuremethodsinterfaces
 import "testing"
 
 func TestArea(t *testing.T) {
-
-	checkArea := func (t *testing.T, form Form, expect float64)  {
-		t.Helper()
-		result := form.Area()
-
-		if result != expect {
-			t.Errorf("expected: %.2f, result: %.2f", expect, result)
-		}
+	testsArea := []struct {
+		form Form
+		expected float64
+	} {
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10}
-		result := circle.Area()
-		expect := 314.15926535897934
-
-		if result != expect {
-			t.Errorf("expected: %.2f result: %.2f", expect, result)
+	for _, tt := range testsArea {
+		result := tt.form.Area()
+		if result != tt.expected {
+			t.Errorf("expected: %.2f, result: %.2f", tt.expected, result)
 		}
-	})
+	}
 }
