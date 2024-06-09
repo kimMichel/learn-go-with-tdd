ad9db18546d31f3c16ec799aee02fc0f39abf0d2
Michel Kim <kim98.michel@gmail.com>
2024-06-08 22:22:34 -0300

    refactor test

--- maps/search_test.go
+++ maps/search_test.go
@@ -8,6 +8,12 @@ func TestSearch(t *testing.T) {
 	result := Search(dictionary, "test")
 	expect := "this is just a test"
 
+	compareString(t, result, expect)
+}
+
+func compareString(t *testing.T, result, expect string) {
+	t.Helper()
+
 	if result != expect {
 		t.Errorf("given: %s, expected: %s, result: %s", "test", expect, result)
 	}
