50a01789991d6f1abd9c4a5fa4bbb0d6965f0412
Michel Kim <kim98.michel@gmail.com>
2024-06-10 23:38:17 -0300

    make more faster using goroutine and select

--- go-select/runner.go
+++ go-select/runner.go
@@ -2,22 +2,22 @@ package goselect
 
 import (
 	"net/http"
-	"time"
 )
 
 func Runner(a, b string) (winner string) {
-	durationA := calculateResponseTime(a)
-	durationB := calculateResponseTime(b)
-
-	if durationA < durationB {
+	select {
+	case <- ping(a):
 		return a
+	case <- ping(b):
+		return b
 	}
-
-	return b
 }
 
-func calculateResponseTime(URL string) time.Duration {
-	start := time.Now()
-	http.Get(URL)
-	return time.Since(start)
+func ping(URL string) chan bool {
+	ch := make(chan bool)
+	go func()  {
+		http.Get(URL)
+		ch <- true
+	}()
+	return ch
 }
