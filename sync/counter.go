12e22d6683c0530b2c56c60e790c3be5505eae01
Michel Kim <kim98.michel@gmail.com>
2024-06-12 12:24:00 -0300

    sync: refactor to use concurrency

--- sync/counter.go
+++ sync/counter.go
@@ -1,10 +1,15 @@
 package sync
 
+import "sync"
+
 type Counter struct {
+	mu sync.Mutex
 	value int
 }
 
 func (c *Counter) Increment() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
--- sync/counter_test.go
+++ sync/counter_test.go
@@ -1,19 +1,44 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("increment the counter 3 times and results the value of 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Increment()
 		counter.Increment()
 		counter.Increment()
 
 		checkCounter(t, counter, 3)
 	})
+
+	t.Run("run on concurrency safety", func(t *testing.T) {
+		expectedCounter := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(expectedCounter)
+
+		for i := 0; i < expectedCounter; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Increment()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		checkCounter(t, counter, expectedCounter)
+	})
 }
 
-func checkCounter(t *testing.T, result Counter, expect int) {
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func checkCounter(t *testing.T, result *Counter, expect int) {
 	t.Helper()
 	if result.Value() != expect {
 		t.Errorf("result: %d, expected: %d", result.Value(), expect)
